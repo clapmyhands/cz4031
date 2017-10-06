31ca96c1b566f2438b42ba173208c00f43a84cc6
Michjk <michjk@gmail.com>
2017-10-06 12:37:48 +0800

    add Proceeding and Inproceeding

---
+++ src/com/common/entity/Proceedings.java
@@ -0,0 +1,35 @@
+package com.common.entity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.UUID;
+
+public class Proceedings extends Publication {
+    private String conf;
+    private String statement = "INSERT INTO proceeding(pubid, pubkey, title, year, month, conf) VALUES " + "(?, ?, ?, ?, ?, ?)";
+
+    public Proceeding() {
+        super();
+        this.conf = null;
+    }
+
+    public void setConf(String conf) {
+        this.conf = conf;
+    }
+
+    @Override
+    PreparedStatement fillPreparedStatement(PreparedStatement pst) throws SQLException {
+        pst = super.fillPreparedStatement(pst);
+        pst.setString(6, this.conf);
+        return pst;
+    }
+
+    @Override
+    public PreparedStatement generateStatement(Connection connection) throws SQLException {
+        PreparedStatement pst = connection.prepareStatement(statement);
+        pst = this.fillPreparedStatement(pst);
+        return pst;
+    }
+
+}
