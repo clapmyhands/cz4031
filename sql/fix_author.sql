8a076ab2738dbc766f0c09dca11ee985c2893f13
clapmyhands <pineapplepie90@gmail.com>
2017-10-14 17:04:29 +0800

    update author and authored to 1 author

---
+++ sql/fix_author.sql
@@ -0,0 +1,25 @@
+-- Query to delete duplicates in author and then update the authored table
+-- Copy author table but without duplicates. Use the author_id with the smallest number.
+WITH result AS (
+    SELECT *, ROW_NUMBER() OVER (PARTITION BY author_name ORDER BY author_id) AS row
+    FROM author
+)
+SELECT author_id, author_name 
+INTO distinct_name
+FROM result
+WHERE row = 1;
+
+UPDATE authored
+SET author_id = aid_updater.new_id
+FROM (
+    SELECT author.author_id as old_id, distinct_name.author_id as new_id
+    FROM author, distinct_name
+    WHERE author.author_name = distinct_name.author_name AND author.author_id != distinct_name.author_id
+) AS aid_updater
+WHERE authored.author_id = aid_updater.old_id;
+
+LTER TABLE distinct_name ADD UNIQUE (author_name);
+
+DROP TABLE author;
+
+ALTER TABLE distinct_name RENAME TO author;
