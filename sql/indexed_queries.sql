5b65d54a5c4eb906de17126348c4a3099bec478b
clapmyhands <pineapplepie90@gmail.com>
2017-10-16 01:19:46 +0800

    change index on date to year

--- sql/indexed_queries.sql
+++ sql/indexed_queries.sql
@@ -1,10 +1,10 @@
-CREATE INDEX book_date ON book(pub_date);
-CREATE INDEX article_date ON article(pub_date);
-CREATE INDEX incollection_date ON incollection(pub_date);
-CREATE INDEX masters_thesis_date ON masters_thesis(pub_date);
-CREATE INDEX phd_thesis_date ON phd_thesis(pub_date);
-CREATE INDEX proceedings_date ON proceedings(pub_date);
-CREATE INDEX inproceedings_date ON inproceedings(pub_date);
+CREATE INDEX book_date ON book(extract(year from pub_date));
+CREATE INDEX article_date ON article(extract(year from pub_date));
+CREATE INDEX incollection_date ON incollection(extract(year from pub_date));
+CREATE INDEX masters_thesis_date ON masters_thesis(extract(year from pub_date));
+CREATE INDEX phd_thesis_date ON phd_thesis(extract(year from pub_date));
+CREATE INDEX proceedings_date ON proceedings(extract(year from pub_date));
+CREATE INDEX inproceedings_date ON inproceedings(extract(year from pub_date));
 
 CREATE INDEX proceedings_conference ON proceedings(booktitle);
 CREATE INDEX inproceedings_conference ON inproceedings(booktitle);
